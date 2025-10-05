36143ccf8ed82c8a0ca5d536bea175af0f70b6ca
Anne Nawrocki <anawrocki.dev@gmail.com>
2025-10-05 16:41:11 +0200

    Section About mise à jour

--- src/containers/About/index.jsx
+++ src/containers/About/index.jsx
@@ -3,11 +3,15 @@ import imgJS from "../../assets/JSonLaptop-600px.webp";
 function About() {
   return (
     <section id="about" className="about">
-      <h2>A propos de moi</h2>
+      {/* <h2>A propos de moi</h2> */}
+      <h2>
+        Mon parcours : De la gestion de projets complexes au Product Building
+        No-code
+      </h2>
       <div className="about-content">
         <img src={imgJS} alt="imgJS" className="about-content__img" />
         <p className="about-content__text">
-          Avec un esprit autant analytique que créatif, mon parcours
+          {/* Avec un esprit autant analytique que créatif, mon parcours
           professionnel est le reflet d’un vif intérêt pour l’innovation et la
           technologie. Après une carrière enrichissante en tant que chercheur et
           développeur de logiciels dans le domaine exigeant de la simulation
@@ -31,7 +35,23 @@ function About() {
           ma vision innovante au sein d’une équipe dynamique où la collaboration
           et la créativité sont au cœur des projets. <br />
           <br />
-          N’hésitez pas à parcourir mon portfolio !
+          N’hésitez pas à parcourir mon portfolio ! */}
+          Mon parcours de scientifique (PhD) et de cheffe de projet m'a appris
+          la rigueur, la pensée analytique et la gestion de la complexité.
+          Aujourd'hui, j'applique ces compétences pour créer des systèmes
+          automatisés et des applications fonctionnelles qui résolvent de vrais
+          problèmes métiers.
+          <br />
+          Mon parcours de scientifique (PhD) et de cheffe de projet m'a appris
+          la rigueur, la pensée analytique et la gestion de la complexité.
+          Aujourd'hui, j'applique ces compétences pour créer des systèmes
+          automatisés et des applications fonctionnelles qui résolvent de vrais
+          problèmes métiers.
+          <br />
+          Pour mettre en pratique cette passion, j'ai récemment développé de A à
+          Z <strong>"MyCRM Freelance"</strong>, un assistant personnel qui
+          automatise la gestion des prospects et le suivi des tâches en
+          connectant Airtable, Make et Tally.
         </p>
       </div>
     </section>
